db808a64feaa31c6061c9e28b0c96cac183f757e
Jim Kalafut <jim@kalafut.net>
2021-10-27 22:46:36 -0700

    Add binary to CLI

--- cmd/lightpatch/lightpatch.go
+++ cmd/lightpatch/lightpatch.go
@@ -15,6 +15,7 @@ var CLI struct {
 		BeforeFile *os.File      `arg help:"Before file"`
 		AfterFile  *os.File      `arg help:"After file"`
 		TimeLimit  time.Duration `name:"t" default:"5s" help:"Max time to build patch."`
+		Binary     bool
 	} `cmd help:"Make a patch file to turn 'before' into 'after'."`
 
 	Apply struct {
@@ -35,7 +36,15 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		patch := lightpatch.MakePatch(before, after)
+		var opts []lightpatch.FuncOption
+		if CLI.Make.Binary {
+			opts = append(opts, lightpatch.WithBase64())
+		}
+
+		patch, err := lightpatch.MakePatch(before, after, opts...)
+		if err != nil {
+			panic(err)
+		}
 		os.Stdout.Write(patch)
 	case "apply <before-file> <patch-file>":
 		before, err := io.ReadAll(CLI.Apply.BeforeFile)
@@ -46,7 +55,12 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		after, err := lightpatch.ApplyPatch(before, patch)
+		var opts []lightpatch.FuncOption
+		if CLI.Make.Binary {
+			opts = append(opts, lightpatch.WithBase64())
+		}
+
+		after, err := lightpatch.ApplyPatch(before, patch, opts...)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "\nerror applying patch: %s\n", err)
 			os.Exit(1)
